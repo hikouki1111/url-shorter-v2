87dc03734ca5b5768e459a9fc36bc823fc1c440e
kometarou1145 <97835611+hikouki1111@users.noreply.github.com>
2025-07-27 17:12:34 +0900

    Update route.ts

--- src/app/[...slug]/route.ts
+++ src/app/[...slug]/route.ts
@@ -1,7 +1,7 @@
 import {NextRequest, NextResponse} from 'next/server'
 import {prisma} from "@/lib/prisma";
 
-export async function GET(req: NextRequest, { params }: { params: { slug?: string[] } }) {
+export async function GET(req: NextRequest, { params }: { params: { slug: string[] } }) {
     const slug = params.slug || []
 
     if (slug.length === 0) {
